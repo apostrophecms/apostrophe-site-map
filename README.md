c8bbb7b31c80665ad8011bba15cb5ca4054a976a
Tom Boutell <tom@punkave.com>
2015-02-22 17:41:57 -0500

    * Now implements Google XML sitemaps * node_modules does not belong in git!

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # apostrophe-site-map
 
-This module generates a sitemap for sites powered by the [Apostrophe](https://apostrophenow.org) CMS. It is intended for use in content strategy when dealing with large sites that have hundreds of pages. **It is not currently intended as Google sitemap. Good organic navigation links are much better because they don't get out of date.**
+This module generates XML and plaintext sitemaps for sites powered by the [Apostrophe](https://apostrophenow.org) CMS.
+
+It serves two purposes: [white-hat SEO](https://support.google.com/webmasters/answer/183668?hl=en&ref_topic=6080646&rd=1) and content strategy.
+
+## SEO with sitemaps
+
+A **frequently-updated and accurate** XML sitemap allows search engines to index your content more quickly and spot new pages immediately. But an out-of-date sitemap is worse than nothing and will damage your site's SEO.
+
+This module attempts to generate a sitemap that includes all of the pages on your site that are visible to the public, including "snippets" such as events, blog posts and people.
+
+## Content strategy
+
+If you're simply looking for a quick visualization of your site's structure for content strategy purposes, this module can help you with that too, via the `--format=text --indent` options.
 
 ## How to use it
 
@@ -14,13 +26,95 @@ To generate a content strategy map of your site:
 
 ```javascript
   {
-    apostrophe-site-map: {}
+    apostrophe-site-map: {
+      // array of page types you do NOT want
+      // to include, even though they are
+      // accessible on the site. You can also
+      // do this at the command line
+      excludeTypes: []
+    }
   }
 ```
 
 3. Run the task:
 
+```
 node app apostrophe:site-map
+```
+
+This generates an XML sitemap and displays it on the console. You can publish it by specifying a location in your project's `public` folder:
+
+```
+node app apostrophe:site-map --file=public/sitemap.xml
+```
+
+**Hint:** set up a cron job to do this nightly.
+
+4. Tell Google about it!
+
+Create a `public/robots.txt` file if you do not already have one and add a sitemap line. Here is a valid example for a site that doesn't have any robot restrictions:
+
+```
+Sitemap: http://EXAMPLE.com/sitemap.xml
+```
+
+You can also have other `robots.txt` directives if you wish.
+
+On Google's next crawl of your site it should pick up on the presence of the sitemap.
+
+**Don't do this once and forget about it. Set up a scheduled task, such as a cron job.**
+
+## Warning: watch out for your custom stuff!
+
+This module does the best it can.
+
+It'll list your published pages, and your published snippets. And it'll rank future events higher than past events.
+
+But it doesn't know anything about the custom URLs, independent of the page tree, that you're generating in your own creative and amazing modules.
+
+If that's a concern for you, create `lib/modules/apostrophe-site-map/index.js` in your project, subclass the module, and override the `custom` method to output information about additional URLs.
+
+It's straightforward: all you have to do is pass Apostrophe page objects, or anything else with a `url` property and a `level` property, to `self.output`.
+
+Here's a simple example. Note the use of `self.host` to get the "stem" of the URL (`http://mysite.com`).
+
+For regular pages in the page tree, `level` starts at `0` (the home page) and increments from there for nested pages. For your own "pages," just keep that in mind. The higher the `level`, the lower the `priority` will be in the XML sitemap.
+
+```javascript
+module.exports = factory;
+
+function factory(options, callback) {
+  return new factory.Construct(options, callback);
+}
+
+factory.Construct = function(options, callback) {
+  var self = this;
+
+  module.exports.Super.call(this, options, null);
+
+  self.custom = function(site, apos, argv, callback) {
+    self.output({
+      url: self.host + '/myspecialplace',
+      level: 4
+    });
+    return callback(null);
+  };
+
+  // Must wait at least until next tick to invoke callback!
+  if (callback) {
+    process.nextTick(function() { return callback(null); });
+  }
+
+};
+```
+
+## Content strategy
+
+You can also use this module just to generate a map of your site for your own study:
+
+```
+node app apostrophe:sitemap --format=text --indent
+```
 
 The result is a very informative depth-first list of pages. Note the use of leading spaces to indicate depth:
 
@@ -35,11 +129,23 @@ The result is a very informative depth-first list of pages. Note the use of lead
 
 You'll want to pipe that to a text file and consider printing it.
 
+*The displayed "depth" of snippets won't correspond directly to the pages they are accessible on. Snippets are output first.* You might want to exclude them when generating content strategy maps.
+
 ## How to exclude stuff
 
 "I don't want thousands of blog posts in there from `apostrophe-blog-2`." OK, so do this:
 
 node app apostrophe:site-map --exclude-types=blogPost
+
+Or do it in `app.js` when configuring the module:
+
+```javascript
+  {
+    apostrophe-site-map: {
+      excludeTypes: [ 'blogPost' ]
+    }
+  }
+```
 
 You may specify multiple page types to exclude, separated by commas.
 
--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 var async = require('async');
 var _ = require('lodash');
-
+var fs = require('fs');
+var moment = require('moment');
 module.exports = factory;
 
 function factory(options, callback) {
@@ -17,14 +18,32 @@ function Construct(options, callback) {
   self._pages = options.pages;
 
   self._apos.on('tasks:register', function(taskGroups) {
-    taskGroups.apostrophe.siteMap = function(apos, argv, callback) {
-      var home, tree;
+    taskGroups.apostrophe.siteMap = function(site, apos, argv, callback) {
+      var format = self.format = argv.format || 'xml';
+      var indent = self.indent = !!argv.indent;
+      var file = self.file = argv.file || '/dev/stdout';
+      var out = self.out = fs.openSync(file, 'w');
+      var home;
       var criteria = {};
-      var req = self._apos.getTaskReq();
+      var today = self.today = moment().format('YYYY-MM-DD');
+      var req = self.req = self._apos.getTaskReq();
+      var excludeTypes = self.excludeTypes = options.excludeTypes || [];
+
+      self.output = output;
+
       if (argv['exclude-types']) {
-        var types = argv['exclude-types'].split(',');
-        criteria.type = { $nin: types };
+        excludeTypes = excludeTypes.concat(argv['exclude-types'].split(','));
+        criteria.type = { $nin: excludeTypes };
       }
+      if (format === 'xml') {
+        fs.writeSync(out, '<?xml version="1.0" encoding="UTF-8"?>\n');
+        fs.writeSync(out, '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n');
+      }
+      var host = site.hostName;
+      if (!host.match(/^https?:/)) {
+        host = 'http://' + host;
+      }
+      self.host = host;
       return async.series({
         home: function(callback) {
           return apos.pages.findOne({ slug: '/' }, function(err, page) {
@@ -38,38 +57,110 @@ function Construct(options, callback) {
             return callback(null);
           });
         },
-        tree: function(callback) {
+        pages: function(callback) {
           return self._pages.getDescendants(req, home, criteria, { orphan: null, depth: 20, fields: { slug: 1, path: 1, rank: 1, level: 1 } }, function(err, children) {
             home.children = children;
             return callback(err);
           });
+        },
+        snippets: function(callback) {
+          return async.eachSeries(self._pages.types, function(type, callback) {
+            if (!_.find(type._modules || [], function(m) {
+              return m.name === 'snippets';
+            })) {
+              // Not derived from snippets
+              return setImmediate(callback);
+            }
+            // You can ignore either the index or the instance,
+            // same result
+            if (_.contains(excludeTypes, type.name)) {
+              return setImmediate(callback);
+            }
+            if (_.contains(excludeTypes, type._instance)) {
+              return setImmediate(callback);
+            }
+            // Paginate through 100 at a time to
+            // avoid slamming memory
+            var done = false;
+            var skip = 0;
+            return async.whilst(
+              function() { return !done; },
+              function(callback) {
+              return type.get(req, { published: true }, { permalink: true, withJoins: false, areas: false, skip: skip, limit: 100 }, function(err, results) {
+                _.each(results.snippets, function(snippet) {
+                  if (!snippet.url) {
+                    // This one has no page to be viewed on
+                    return;
+                  }
+                  snippet.url = host + snippet.url;
+                  // Results in a reasonable priority relative
+                  // to regular pages
+                  snippet.level = 3;
+                  // Future events are interesting,
+                  // past events are boring
+                  if (snippet.startDate) {
+                    if (snippet.startDate > today) {
+                      snippet.level--;
+                    } else {
+                      snippet.level++;
+                    }
+                  }
+                  output(snippet);
+                });
+                if (!results.snippets.length) {
+                  done = true;
+                } else {
+                  skip += results.snippets.length;
+                }
+                return callback(null);
+              });
+            }, callback);
+          }, callback);
+        },
+        custom: function(callback) {
+          return self.custom(site, apos, argv, callback);
         }
       }, function(err) {
         if (err) {
           return callback(err);
         }
         output(home);
-        return callback(null);
-        function output(page) {
-          var s = '';
-          var i;
-          for (i = 0; (i < page.level); i++) {
-            s += '  ';
-          }
-          s += page.slug;
-          console.log(s);
-          _.each(page.children, function(page) {
-            output(page);
-          });
+        if (format === 'xml') {
+          fs.writeSync(out, '</urlset>\n');
         }
+        fs.closeSync(out);
+        return callback(null);
       });
+      function output(page) {
+        if (format === 'text') {
+          if (indent) {
+            var i;
+            for (i = 0; (i < page.level); i++) {
+              fs.writeSync(out, '  ');
+            }
+          }
+          fs.writeSync(out, page.slug);
+        } else {
+          var url = page.url || host + site.prefix + page.slug;
+          fs.writeSync(out, '  <url><priority>' + (1.0 - page.level / 10) + '</priority><changefreq>daily</changefreq><loc>' + url + '</loc></url>\n');
+        }
+        _.each(page.children, function(page) {
+          output(page);
+        });
+      }
     };
   });
 
-  // Invoke the callback. This must happen on next tick or later!
-  return process.nextTick(function() {
-    return callback(null);
-  });
+  self.custom = function(site, apos, argv, callback) {
+    return setImmediate(callback);
+  };
+
+  if (callback) {
+    // Invoke the callback. This must happen on next tick or later!
+    return process.nextTick(function() {
+      return callback(null);
+    });
+  }
 }
 
 // Export the constructor so others can subclass
